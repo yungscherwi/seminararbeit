1bbd83716953a2987c17d14421e220ba7eb7e9c0
Leon <leonscherweit@Leons-MBP.fritz.box>
2018-03-16 19:34:10 +0100

    Ionic Framework mit Google Maps Javascript API integriert
    Basierend auf diesem Tutorial:
    
    Ionic 3: Google Maps SDK (https://www.youtube.com/watch?v=0Ue6fNPOdB4)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core'; //ViewChild und ElementRef ergänzt
 import { NavController } from 'ionic-angular';
+
+declare var google: any; //Keine Errors mit google als variable
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  @ViewChild('map') mapRef: ElementRef; //Verbindung zum map Element
+  map: any;
   constructor(public navCtrl: NavController) {
+      }
 
+    ionViewDidLoad(){
+      this.showMap();
+    }
+
+    showMap(){
+      //Location - lat long
+      const location = new google.maps.LatLng(51.507531, -0.127758);
+
+
+    //Map options
+    const options ={
+      center: location,
+      zoom: 10
+    }
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
   }
+
 
 }
