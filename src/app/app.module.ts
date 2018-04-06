12b080fa6804e0fd3240dbd761136b3ab90ee65f
Lucas Tiedemann <lucas95t@gmail.com>
2018-04-06 14:29:16 +0200

    ohne_places

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+//import { AboutPage } from '../pages/about/about';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { MylifePage } from '../pages/mylife/mylife'; //Import für Mylife tab
 import { EinstellungenPage } from '../pages/einstellungen/einstellungen';
-
+import { Geolocation } from '@ionic-native/geolocation'; //Import für GPS
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ProgressBarComponent } from '../components/progress-bar/progress-bar'; //Importiert Progress-Bar, zudem auch unter declarations einfügen
@@ -37,6 +37,7 @@ import { ProgressBarComponent } from '../components/progress-bar/progress-bar'; 
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core'; //ViewChild und ElementRef ergänzt
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation'; //Import für GPS
 
 declare var google: any; //Keine Errors mit google als variable
 @Component({
@@ -12,29 +13,44 @@ export class HomePage {
   loadProgress: number;
   reichweite: number;
   kmStand: number;
-  constructor(public navCtrl: NavController) {
+
+  constructor(public navCtrl: NavController, public geo: Geolocation) { //Obejekt für Location
       }
 
     ionViewDidLoad(){
       this.showMap();
-      this.loadProgress = 50; //Input für den Akkustand
-      this.reichweite = 545; //Input für reichweite
+      this.loadProgress = 100; //Input für den Akkustand
+      this.reichweite = 345; //Input für reichweite
       this.kmStand = 5655; //Input für kmStand
-    }
+}
 
     showMap(){
-      //Location - lat long
-      const location = new google.maps.LatLng(51.545483, 9.905548);
-
-
+    this.geo.getCurrentPosition().then( pos => {   //Frag aktuelle Position ab
+    this.lat = pos.coords.latitude;
+    this.lng = pos.coords.longitude;
+    var current_location = new google.maps.LatLng(this.lat, this.lng);  //besetzt Variable current_location mit aktuellen LatLng
     //Map options
-    const options ={
-      center: location,
+  /*  const options ={
+      center: current_location,
       zoom: 12
-    }
+    }*/
+//  this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: current_location,
+    zoom: 12
+  });
+//Marler hinzufügen
+  let marker: google.maps.Marker = new google.maps.Marker({  //setzt Marker auf aktuelle Position
+      map: map,
+      position: current_location,
+    })
+    let marker2: google.maps.Marker = new google.maps.Marker({
+      map: map,
+      position: new google.maps.LatLng(51.5322716,9.9298304)
+    })
 
-    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
-  }
 
+    }).catch( err => console.log(err)); //Fehler in Konsolenausgabe auffangen
+}
 
 }
